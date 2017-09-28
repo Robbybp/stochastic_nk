c1bc3927de33d1dfbdc67cbbaeebad5153d55de8
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2017-09-28 14:53:25 -0600

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -1,29 +1,28 @@
- using JuMP
- using PowerModels
- using CPLEX
+using JuMP
+using PowerModels
+using CPLEX
  
- PMs = PowerModels
+PMs = PowerModels
  
- include("parse.jl")
- include("utils.jl")
- include("subproblem.jl")
+include("parse.jl")
+include("utils.jl")
+include("subproblem.jl")
  
- # setting up configuration for the run
- config = parse_commandline()
- config["casefile"] = string(config["path"], config["file"])
- config["numscenarios"] = config["batchsize"] * config["numbatches"]
+# setting up configuration for the run
+config = parse_commandline()
+config["casefile"] = string(config["path"], config["file"])
+config["numscenarios"] = config["batchsize"] * config["numbatches"]
+
+for (arg, val) in config
+    println(">> $arg => $val")
+end
  
- for (arg, val) in config
-     println(">> $arg => $val")
- end
+data = PMs.parse_file(config["casefile"])
+ref = PMs.build_ref(data)
  
- data = PMs.parse_file(config["casefile"])
- ref = PMs.build_ref(data)
+config["scenariofile"] = string(config["path"], "scenario_data/case", length(keys(ref[:nw][0][:bus])), "_scenarios_unique.txt")
  
- config["scenariofile"] = string(config["path"], "scenario_data/case", length(keys(ref[:nw][0][:bus])), "_scenarios_unique.txt")
- 
- # fetch the scenarios for the run
- (!isfile(config["scenariofile"])) && (println(">> scenario file does not exist, quitting program ..."); quit())
- scenarios = fetch_scenarios(config)
- @assert size(scenarios)[1] == config["batchsize"]
- 
+# fetch the scenarios for the run
+(!isfile(config["scenariofile"])) && (println(">> scenario file does not exist, quitting program ..."); quit())
+scenarios = fetch_scenarios(config)
+@assert size(scenarios)[1] == config["batchsize"] 
