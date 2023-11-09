ee9535411353a7f35e8362a1a21dd40927e9016c
kaarthiksundar <kaarthik01sundar@gmail.com>
2023-11-09 16:01:57 -0700

    scenario generation complete

--- data/scenario_data/tar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-tar -zcvf pglib_opf_case14_ieee.tar.gz pglib_opf_case14_ieee.json 
-tar -zcvf pglib_opf_case240_pserc.tar.gz pglib_opf_case240_pserc.json 
-tar -zcvf CATS.tar.gz CATS.json 
-rm -f *.json
--- src/scenario_generation/generate_scenarios.jl
+++ src/scenario_generation/generate_scenarios.jl
@@ -6,10 +6,18 @@ using DataFrames
 using CSV
 using Random
 using JSON
+using ZipFile
 
 PMs = PowerModels
 PMs.silence()
 
+""" code for all the scenario generation runs 
+
+julia --project=. src/scenario_generation/generate_scenarios.jl 
+julia --project=. src/scenario_generation/generate_scenarios.jl --case CATS.m --use_clusters --num_min_outages 15 --num_max_outages 30 --num_scenarios 1000
+julia --project=. src/scenario_generation/generate_scenarios.jl --case pglib_opf_case240_pserc.m --use_clusters --num_min_outages 10 --num_max_outages 20 --num_scenarios 1000
+
+"""
 
 function parse_commandline()
     s = ArgParseSettings()
@@ -35,10 +43,15 @@ function parse_commandline()
         arg_type = Int
         default = 200
 
-        "--num_outages", "-l"
+        "--num_max_outages"
         help = "maximum number of line/gen outages in each scenario"
         arg_type = Int 
         default = 4 
+
+        "--num_min_outages"
+        help = "minimum number of line/gen outages in each scenario" 
+        arg_type = Int 
+        default = 1
 
         "--use_clusters"
         help = "flag to use the clustering data for the case file"
@@ -79,20 +92,20 @@ function get_filenames_with_paths(params)
     case_name = chop(params["case"], tail = length(".m"))
     matpower_file = params["data_path"] * "matpower/" * params["case"]
     scenario_file = params["data_path"] * "scenario_data/" * case_name * ".json"
-    tar_file = params["data_path"] * "scenario_data/" * case_name * ".tar.gz"
+    zip_file = params["data_path"] * "scenario_data/" * case_name * ".zip"
     cluster_file = params["data_path"] * "gis/" * case_name * "_cluster.csv"
     return (mp_file = matpower_file, 
         scenario_file = scenario_file, 
         cluster_file = cluster_file,
-        tar_file = tar_file)
+        zip_file = zip_file)
 end 
 
 function run(config, files)
     mp_file = files.mp_file 
     scenario_file = files.scenario_file 
     cluster_file = files.cluster_file
-    tar_file = files.tar_file
-    (isfile(scenario_file)) && (@error "$scenario_file exists, quitting"; exit())
+    zip_file = files.zip_file
+    (isfile(scenario_file)) && (@warn "$scenario_file exists, overwriting")
     (config["use_clusters"] && !isfile(cluster_file)) && (@error "$cluster_file does not exists, quitting"; exit())
 
     data = PMs.parse_file(mp_file)
@@ -105,7 +118,7 @@ function run(config, files)
         gen_ids = ref[:gen] |> keys
         components = ["line", "gen"]
 
-        num_outages = config["num_outages"]
+        num_outages = config["num_max_outages"]
         scenarios = Dict()
         for i in 1:config["num_scenarios"]
             scenarios[i] = Dict("branch" => [] , "gen" => []) 
@@ -122,7 +135,8 @@ function run(config, files)
             JSON.print(f, scenarios, 2) 
         end
 
-        Base.run(`tar -zcvf $tar_file --absolute-paths $scenario_file`)
+        zip(zip_file, scenario_file)
+        # Base.run(`tar -zcvf $tar_file --absolute-paths $(split(scenario_file, "/")[end])`)
         Base.run(`rm -f $scenario_file`)
     else 
         df = DataFrame(CSV.File(cluster_file))
@@ -134,7 +148,63 @@ function run(config, files)
             buses = cluster[!, "bus_id"] |> collect 
             cluster_info[i] = buses
         end 
+        scenario_files = []
+
+        for (cluster_id, buses) in cluster_info 
+            gen_ids = filter(x -> last(x)["gen_bus"] in buses, ref[:gen]) |> keys 
+            line_ids = filter(x -> last(x)["f_bus"] in buses && last(x)["t_bus"] in buses, ref[:branch]) |> keys
+            components = ["line", "gen"]
+            num_outages = range(config["num_min_outages"], config["num_max_outages"])
+            
+            scenarios = Dict() 
+            for i in 1:config["num_scenarios"]
+                scenarios[i] = Dict("branch" => [] , "gen" => []) 
+                k = rand(num_outages)
+                for _ in 1:k
+                    component = rand(components)
+                    (component == "line") && (push!(scenarios[i]["branch"], rand(line_ids)))
+                    (component == "gen") && (push!(scenarios[i]["gen"], rand(gen_ids)))
+                end 
+                unique!(scenarios[i]["branch"])
+                unique!(scenarios[i]["gen"])
+            end 
+            
+            new_scenario_file = chop(scenario_file, tail = length(".json")) * "_$(cluster_id).json"
+            push!(scenario_files, new_scenario_file)
+            open(new_scenario_file, "w") do f 
+                JSON.print(f, scenarios, 2) 
+            end
+        end 
+        zip(zip_file, scenario_files)
+        map(x -> Base.run(`rm -f $x`), scenario_files)
     end 
+end 
+
+function zip(zip, files::Vector)
+    compress = true 
+    zdir = ZipFile.Writer(zip)
+    for file in files 
+        f = open(file, "r")
+        content = read(f, String)
+        close(f)
+
+        zf = ZipFile.addfile(zdir, split(file, "/")[end]; method=(compress ? ZipFile.Deflate : ZipFile.Store));
+        write(zf, content)
+    end 
+    close(zdir)
+end 
+
+
+function zip(zip, file)
+    compress = true
+    zdir = ZipFile.Writer(zip) 
+    f = open(file, "r")
+    content = read(f, String)
+    close(f)
+
+    zf = ZipFile.addfile(zdir, split(file, "/")[end]; method=(compress ? ZipFile.Deflate : ZipFile.Store));
+    write(zf, content)
+    close(zdir)
 end 
 
 main()
