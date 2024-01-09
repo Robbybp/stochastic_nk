197b2f55c3741c4a76ad89db8e424d68e201742f
kaarthiksundar <kaarthik01sundar@gmail.com>
2024-01-09 12:46:51 -0700

    adds rts_gmlc_det script

--- scripts/runscript.sh
+++ scripts/runscript.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #
 #SBATCH --ntasks=1
-#SBATCH --nodes=1
+#SBATCH --qos=normal --time=10:00:00
+#SBATCH --exclusive
+#
+#This recomend to minimize clashes with other users
 #SBATCH --partition=scaling
-#SBATCH --time=72:00:00
+#report email setup
+#SBATCH --mail-user kaarthik@lanl.gov
+#SBATCH --mail-type=ALL
 
-
-srun julia main.jl "$@"
+srun julia --project=. src/main.jl "$@"
